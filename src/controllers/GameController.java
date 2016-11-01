43548d3f666cfb41e60b1626a2a1faf8b793bd11
Windovvsill <svezer@live.ca>
2016-10-31 21:53:51 -0700

    Added game stages and controllers

--- src/controllers/GameController.java
+++ src/controllers/GameController.java
@@ -1,6 +1,26 @@
 package controllers;
 
+import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.*;
 
-public class GameController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+    public void p(String s) {
+        System.out.println(s);
+    }
 }
--- src/controllers/MainMenuController.java
+++ src/controllers/MainMenuController.java
@@ -3,20 +3,79 @@ package controllers;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.*;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Created by haunter on 31/10/16.
  */
-public class MainMenuController {
+public class MainMenuController implements Initializable {
+
     @FXML
     Button createGameButton, joinGameButton, exitButton;
 
-    public void initialize() {
-        createGameButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override public void handle(ActionEvent e) {
-                System.out.println("PPPPPPPPPPP");
-            }
-        });
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        createGameButton.setOnAction((event -> createGameButtonHandler(event)));
+        joinGameButton.setOnAction((event -> joinGameButtonHandler(event)));
+        exitButton.setOnAction((event -> exitButtonHandler()));
+    }
+
+    public void createGameButtonHandler(ActionEvent event) {
+        // Code for creating a game goes here
+        launchGame(event);
+    }
+    public void  launchGame(ActionEvent event) {
+        // Create the stage for the game and show it
+        showGameStage();
+        // Close the main menu
+        closeMenuStage(event);
+
+    }
+
+    public void showGameStage() {
+        try {
+            p("Initializing Game Controller");
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainMenuController.class.getResource("../views/Game.fxml"));
+            AnchorPane anchorPane = loader.load();
+            Stage gameStage = new Stage();
+            gameStage.setTitle("Modify Parameters");
+            Scene gameScene = new Scene(anchorPane);
+            gameStage.initStyle(StageStyle.UTILITY);
+            gameStage.setResizable(false);
+            gameStage.setScene(gameScene);
+
+            gameStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void closeMenuStage(ActionEvent event) {
+        Node  source = (Node)  event.getSource();
+        Stage stage  = (Stage) source.getScene().getWindow();
+        stage.close();
+    }
+    public void joinGameButtonHandler(ActionEvent event) {
+        // Code for joining a game goes here
+    }
+    public void exitButtonHandler() {
+        System.exit(0);
+    }
+    public void exitButtonHandler(ActionEvent event) {
+        exitButtonHandler();
+    }
+    public void p(String s) {
+        System.out.println(s);
     }
 }
