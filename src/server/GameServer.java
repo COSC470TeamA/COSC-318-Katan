ffea748efef07b7a1cff5185fa29a0e035c19de2
Breanne Wiebe <brewiebe@gmail.com>
2016-12-05 16:38:50 -0800

    initialization phase

--- src/server/GameServer.java
+++ src/server/GameServer.java
@@ -14,13 +14,13 @@ import java.util.List;
  * Created by steve on 2016-11-09.
  */
 public class GameServer {
-    private ServerSocket socket;
-
-    //Map<Integer, Player> clients = new TreeMap<>();
+    private static boolean gameStarted = false;
     boolean victoryIsReached = false;
     int VICTORY_CONDITION = 4;
 
     private static final List<GameServerThread> clients = Collections.synchronizedList(new ArrayList<GameServerThread>());
+
+    private static ArrayList<String> initialMessages = new ArrayList<>();
 
 
     public static void main(String[] args) throws Exception {
@@ -77,11 +77,58 @@ public class GameServer {
         return null;
     }
 
+
+
     private static void sendToAllClients(String message){
-        if (message.startsWith("sg") && clients.size() < 2) return;
+        if (message.startsWith("sg") && clients.size() == 2 && !gameStarted) {
+            gameStarted = true;
+            initializeGame();
+            return;
+        }
+
         for(GameServerThread client : clients) {
             client.sendMessage(message);
         }
+    }
+
+    private static void initializeGame() {
+        ArrayList<ArrayList<String>> initialMessages = getInitialMessages();
+
+        for(int i = 0; i < 2; i++) {
+            ArrayList<String> messages = initialMessages.get(i);
+
+            for(String string : messages) {
+                string = string + clients.get(i).player.getColor();
+
+                if (string.startsWith("dh")) {
+                    clients.get(i).addHouseToClient(string);
+                }
+
+                for(GameServerThread client : clients) {
+                    client.sendMessage(string);
+                }
+            }
+        }
+    }
+
+    private static ArrayList<ArrayList<String>> getInitialMessages() {
+        ArrayList<String> client1Messages = new ArrayList<>();
+        client1Messages.add("dr:256.20508075688775:125.0:0.0:");
+        client1Messages.add("dh:256.20508075688775:100.0:0,3!1,2!1,3!:");
+        client1Messages.add("dr:300.20508075688775:208.0:0.0:");
+        client1Messages.add("dh:298.20508075688775:171.0:2,4!1,3!2,3!:");
+
+        ArrayList<String> client2Messages = new ArrayList<>();
+        client2Messages.add("dr:111.49226701332081:242.88972603279078:60.0:");
+        client2Messages.add("dh:88.20508075688775:248.0:3,1!3,0!2,1!:");
+        client2Messages.add("dr:194.4800670631703:308.8493623411972:120.0:");
+        client2Messages.add("dh:210.20508075688775:322.0:4,2!3,2!4,3!:");
+
+        ArrayList<ArrayList<String>> startupMessages = new ArrayList<>();
+        startupMessages.add(client1Messages);
+        startupMessages.add(client2Messages);
+
+        return startupMessages;
     }
 }
 
